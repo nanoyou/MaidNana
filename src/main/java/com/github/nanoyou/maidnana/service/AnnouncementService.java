3be6025209c832a433727cc886b3bb166a46d0ea
AkagawaTsurunaki <965145619@qq.com>
2022-08-31 23:08:33 +0800

    refactor: AnnouncementService.java 单例模式

--- src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
+++ src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
@@ -1,4 +1,8 @@
 package com.github.nanoyou.maidnana.service;
 
 public class AnnouncementService {
+    private final static AnnouncementService instance = new AnnouncementService();
+    public final static AnnouncementService getInstance() {
+        return instance;
+    }
 }
