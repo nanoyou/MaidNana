9b4f17c774dc714a6a7a491ca433ac5843052ee5
AkagawaTsurunaki <965145619@qq.com>
2022-09-01 12:03:36 +0800

    feat:SERVICE层添加方法(待完成)

--- src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
+++ src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
@@ -1,9 +1,14 @@
 package com.github.nanoyou.maidnana.service;
 
 import com.github.nanoyou.maidnana.dao.TemplateDao;
+import com.github.nanoyou.maidnana.entity.Announcement;
+import com.github.nanoyou.maidnana.entity.Body;
+import com.github.nanoyou.maidnana.entity.Trigger;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.TestOnly;
+
+import java.util.UUID;
 
 public class AnnouncementService {
     @Getter
@@ -11,8 +16,89 @@ public class AnnouncementService {
 
     private final TemplateDao templateDao = TemplateDao.getInstance();
 
-    public void test() {
+    /**
+     * @return
+     */
+    public Announcement create() {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @param announcementID
+     * @return
+     */
+    public Announcement get(UUID announcementID) {
+        return null;
+    }
+
+    /**
+     * @param announcementID
+     * @return
+     */
+    public Announcement delete(UUID announcementID) {
+        return null;
+    }
+
+    /**
+     * @param announcementID
+     * @param groupID
+     */
+    public void addGroup(UUID announcementID, long groupID) {
 
     }
 
+    /**
+     * @param announcementID
+     * @param groupID
+     */
+    public void removeGroup(UUID announcementID, long groupID) {
+
+    }
+
+    /**
+     * @param announcementID
+     * @param trigger
+     */
+    public void addTrigger(UUID announcementID, Trigger trigger) {
+
+    }
+
+    /**
+     * @param announcementID
+     * @param triggerID
+     */
+    public void removeTrigger(UUID announcementID, UUID triggerID) {
+
+    }
+
+    /**
+     * @param announcementID
+     * @param triggerID
+     */
+    public void increaseTrigger(UUID announcementID, UUID triggerID) {
+
+    }
+
+    /**
+     * @param announcementID
+     * @param body
+     */
+    public void setBody(UUID announcementID, Body body) {
+
+    }
+
+    /**
+     * @param announcementID
+     */
+    public void enable(UUID announcementID) {
+
+    }
+
+    /**
+     * @param announcementID
+     */
+    public void disable(UUID announcementID) {
+
+    }
 }
---
+++ src/main/java/com/github/nanoyou/maidnana/service/TemplateService.java
@@ -0,0 +1,42 @@
+package com.github.nanoyou.maidnana.service;
+
+import com.github.nanoyou.maidnana.entity.Template;
+import lombok.Getter;
+
+import java.util.UUID;
+
+public class TemplateService {
+    
+    @Getter
+    private final static TemplateService instance = new TemplateService();
+
+    /**
+     * @return
+     */
+    public Template create() {
+        return null;
+    }
+
+    /**
+     * @param templateID
+     */
+    public void delete(UUID templateID) {
+
+    }
+
+    /**
+     * @param templateID
+     */
+    public void get(UUID templateID) {
+
+    }
+
+    /**
+     * @param templateID
+     * @param template
+     */
+    public void modify(UUID templateID, String template) {
+
+    }
+
+}
