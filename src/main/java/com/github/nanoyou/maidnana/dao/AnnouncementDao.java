3822036f51a2c786e7e4e28335ab41d2a4362efa
AkagawaTsurunaki <965145619@qq.com>
2022-09-01 11:50:46 +0800

    feat: DAO层添加方法(待完成)

--- src/main/java/com/github/nanoyou/maidnana/dao/AnnouncementDao.java
+++ src/main/java/com/github/nanoyou/maidnana/dao/AnnouncementDao.java
@@ -1,4 +1,60 @@
 package com.github.nanoyou.maidnana.dao;
 
+import com.github.nanoyou.maidnana.entity.Announcement;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
 public class AnnouncementDao {
+    @Getter
+    private final static AnnouncementDao instance = new AnnouncementDao();
+
+    /**
+     * 增加一个Announcement
+     *
+     * @param a
+     */
+    public Announcement add(Announcement a) {
+        // TODO: 增加一个Announcement
+        return null;
+    }
+
+    /**
+     * 通过uuid查找返回Announcement
+     *
+     * @param uuid
+     * @return
+     */
+    public Announcement get(UUID uuid) {
+        // TODO: 通过uuid查找返回Announcement
+        return null;
+    }
+
+    /**
+     * @param a
+     * @return
+     */
+    public Announcement modify(Announcement a) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @param uuid
+     * @return
+     */
+    public Announcement delete(UUID uuid) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public List<Announcement> getAll() {
+        // TODO:
+        return null;
+    }
+
 }
--- src/main/java/com/github/nanoyou/maidnana/dao/TemplateDao.java
+++ src/main/java/com/github/nanoyou/maidnana/dao/TemplateDao.java
@@ -1,8 +1,57 @@
 package com.github.nanoyou.maidnana.dao;
 
+import com.github.nanoyou.maidnana.entity.Template;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
 
 public class TemplateDao {
     @Getter
     private final static TemplateDao instance = new TemplateDao();
+
+    /**
+     * @param t
+     * @return
+     */
+    public Template add(Template t) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @param uuid
+     * @return
+     */
+    public Template get(UUID uuid) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @param t
+     * @return
+     */
+    public Template modify(Template t) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @param uuid
+     * @return
+     */
+    public Template delete(UUID uuid) {
+        // TODO:
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public List<Template> getAll() {
+        // TODO:
+        return null;
+    }
+
 }
