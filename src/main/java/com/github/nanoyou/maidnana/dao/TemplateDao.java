99db06eb1945df457ab50eed7e1790ae3c91c4c4
AkagawaTsurunaki <965145619@qq.com>
2022-09-01 11:19:06 +0800

    feat: 新增TemplateDao.java.在Service层添加了一些测试函数.

---
+++ src/main/java/com/github/nanoyou/maidnana/dao/TemplateDao.java
@@ -0,0 +1,8 @@
+package com.github.nanoyou.maidnana.dao;
+
+import lombok.Getter;
+
+public class TemplateDao {
+    @Getter
+    private final static TemplateDao instance = new TemplateDao();
+}
--- src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
+++ src/main/java/com/github/nanoyou/maidnana/service/AnnouncementService.java
@@ -1,10 +1,18 @@
 package com.github.nanoyou.maidnana.service;
 
+import com.github.nanoyou.maidnana.dao.TemplateDao;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.TestOnly;
 
 public class AnnouncementService {
     @Getter
     private final static AnnouncementService instance = new AnnouncementService();
+
+    private final TemplateDao templateDao = TemplateDao.getInstance();
+
+    public void test() {
+
+    }
 
 }
