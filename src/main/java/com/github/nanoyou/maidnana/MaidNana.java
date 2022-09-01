777d08dae7d3a7484162f8244bf02315e7f4d70a
PermissionDog <455662503@qq.com>
2022-09-01 20:49:23 +0800

    feat: 添加命令权限检测

--- src/main/java/com/github/nanoyou/maidnana/MaidNana.java
+++ src/main/java/com/github/nanoyou/maidnana/MaidNana.java
@@ -1,8 +1,14 @@
 package com.github.nanoyou.maidnana;
 
+import com.github.nanoyou.maidnana.controller.AnnouncementController;
+import kotlin.Lazy;
+import kotlin.LazyKt;
+import net.mamoe.mirai.console.permission.*;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import net.mamoe.mirai.contact.Member;
+import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
@@ -28,6 +34,28 @@ import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 public final class MaidNana extends JavaPlugin {
     public static final MaidNana INSTANCE = new MaidNana();
+    // 公告的权限节点
+    private final Lazy<Permission> announcementPermission = LazyKt.lazy(() -> {
+        try {
+            return PermissionService.getInstance().register(
+                    permissionId("announcement"),
+                    "公告相关权限",
+                    getParentPermission()
+            );
+        } catch (PermissionRegistryConflictException e) {
+            throw new RuntimeException(e);
+        }
+    });
+    // 测试是否用户有权限
+    public boolean hasAnnouncementPermission(User user) {
+        PermitteeId pid;
+        if (user instanceof Member) {
+            pid = new AbstractPermitteeId.ExactMember(((Member) user).getGroup().getId(), user.getId());
+        } else {
+            pid = new AbstractPermitteeId.ExactUser(user.getId());
+        }
+        return PermissionService.hasPermission(pid, announcementPermission.getValue());
+    }
     private MaidNana() {
         super(new JvmPluginDescriptionBuilder("com.github.nanoyou.maidnana", "0.1.0")
                 .info("EG")
@@ -36,7 +64,6 @@ public final class MaidNana extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("日志");
         EventChannel<Event> eventChannel = GlobalEventChannel.INSTANCE.parentScope(this);
         eventChannel.subscribeAlways(GroupMessageEvent.class, g -> {
             //监听群消息
@@ -48,9 +75,17 @@ public final class MaidNana extends JavaPlugin {
                 }
             }
         });
-        eventChannel.subscribeAlways(FriendMessageEvent.class, f -> {
-            //监听好友消息
-            getLogger().info(f.getMessage().contentToString());
+        AnnouncementController announcementController = AnnouncementController.getInstance();
+
+        // 过滤出所有有权限用户发的的消息
+        EventChannel<Event> channel = eventChannel.filter(evt -> {
+            if (!(evt instanceof FriendMessageEvent)) {
+                return false;
+            }
+            return hasAnnouncementPermission(((FriendMessageEvent) evt).getSender());
         });
+
+        // 注册消息
+        channel.subscribeAlways(FriendMessageEvent.class, announcementController::newAnnouncement);
     }
 }
--- src/main/java/com/github/nanoyou/maidnana/controller/AnnouncementController.java
+++ src/main/java/com/github/nanoyou/maidnana/controller/AnnouncementController.java
@@ -1,4 +1,20 @@
 package com.github.nanoyou.maidnana.controller;
 
+import lombok.Getter;
+import net.mamoe.mirai.console.permission.PermissionService;
+import net.mamoe.mirai.event.events.FriendMessageEvent;
+
 public class AnnouncementController {
+    private static final AnnouncementController instance = new AnnouncementController();
+
+    public static AnnouncementController getInstance() {
+        return instance;
+    }
+
+    public void newAnnouncement(FriendMessageEvent event) {
+        if (!event.getMessage().contentToString().startsWith("新建公告")) {
+            return;
+        }
+        event.getSender().sendMessage("新建公告!");
+    }
 }
