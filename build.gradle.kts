6486026a635f6a282e0e000cc26ba006356af5ec
PermissionDog <455662503@qq.com>
2022-09-02 22:01:06 +0800

    build: 修改 lombok 的引入方式

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.20")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
     implementation("com.google.code.gson:gson:2.9.1")
 }
