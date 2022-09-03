a74e5eb9e0bcac788f6d54bcaad646dacd43c4b7
PermissionDog <455662503@qq.com>
2022-09-03 12:53:20 +0800

    feat: 添加命令用法常量

--- src/main/java/com/github/nanoyou/maidnana/constant/MaidNanaConstant.java
+++ src/main/java/com/github/nanoyou/maidnana/constant/MaidNanaConstant.java
@@ -1,4 +1,5 @@
 package com.github.nanoyou.maidnana.constant;
 
 public class MaidNanaConstant {
+    public static final String VERSION = "0.1.0";
 }
--- src/main/java/com/github/nanoyou/maidnana/constant/Usage.java
+++ src/main/java/com/github/nanoyou/maidnana/constant/Usage.java
@@ -1,7 +1,10 @@
 package com.github.nanoyou.maidnana.constant;
 
 public class Usage {
-    public static final String NEW_TEMPLATE = "新建模板";
-    public static final String DELETE_TEMPLATE = "删除模板 <模板UUID>";
-    public static final String MODIFY_TEMPLATE = "修改模板 <模板UUID>\n<模板体(多行)>";
+    public static final String USAGE = "MaidNana - ver " + MaidNanaConstant.VERSION + "\n命令列表如下, [] 代表可选参数, <> 代表必选参数, - 后为使用说明";
+    public static final String NEW_ANNOUNCEMENT = "新建公告 [公告别名] - 新建公告, 可指定别名便于记忆";
+    public static final String SELECT_ANNOUNCEMENT = "选择公告 <公告UUID | 公告别名> - 为后续命令指定公告";
+    public static final String NEW_TEMPLATE = "新建模板 [模板别名]\n<模板体(多行)> - 新建公告模板, 使用'$'定义变量, 如$name$";
+    public static final String DELETE_TEMPLATE = "删除模板 <模板UUID | 模板别名> - 删除公告模板";
+    public static final String MODIFY_TEMPLATE = "修改模板 <模板UUID | 模板别名>\n<模板体(多行)> - 修改指定的模板";
 }
