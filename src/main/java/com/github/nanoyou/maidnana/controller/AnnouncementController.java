b7cd263ea2d1e59674ab7c6f562a31b658292a42
AkagawaTsurunaki <965145619@qq.com>
2022-09-05 17:33:47 +0800

    chore: 增加一个测试项：新增公告

--- src/main/java/com/github/nanoyou/maidnana/controller/AnnouncementController.java
+++ src/main/java/com/github/nanoyou/maidnana/controller/AnnouncementController.java
@@ -112,6 +112,7 @@ public class AnnouncementController {
      *
      * @param event 好友信息事件
      */
+    //TODO:TEST
     public void newAnnouncement(FriendMessageEvent event) {
         if (!event.getMessage().contentToString().startsWith("新建公告")) {
             return;
