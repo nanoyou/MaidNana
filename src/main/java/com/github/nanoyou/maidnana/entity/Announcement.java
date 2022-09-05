bd0b2b23b7499894207b70dfc3feef7e350a1178
PermissionDog <455662503@qq.com>
2022-09-05 19:23:52 +0800

    refactor: 添加实体类构造方法中部分变量初始化

--- src/main/java/com/github/nanoyou/maidnana/entity/Announcement.java
+++ src/main/java/com/github/nanoyou/maidnana/entity/Announcement.java
@@ -3,6 +3,7 @@ package com.github.nanoyou.maidnana.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,6 +13,10 @@ import java.util.UUID;
  */
 @Data
 public class Announcement implements Aliasable {
+    public Announcement() {
+        groups = new ArrayList<>();
+        triggers = new ArrayList<>();
+    }
     private UUID uuid;
     private String alias;
 
